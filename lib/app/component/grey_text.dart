4b7f8d1c1a80449c9f3307ce26e63c0ccea51732
Alif Zakya Rafiq <alifzakya.r@gmail.com>
2023-06-14 10:37:44 +0700

    add textAlign property

--- lib/app/component/grey_text.dart
+++ lib/app/component/grey_text.dart
@@ -6,16 +6,19 @@ class GreyText extends StatelessWidget {
     required this.text,
     this.size,
     this.bold,
+    this.textAlign,
   }) : super(key: key);
 
   final String text;
   final double? size;
   final FontWeight? bold;
+  final TextAlign? textAlign;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      textAlign: textAlign,
       style: TextStyle(color: Colors.grey, fontSize: size, fontWeight: bold),
     );
   }
