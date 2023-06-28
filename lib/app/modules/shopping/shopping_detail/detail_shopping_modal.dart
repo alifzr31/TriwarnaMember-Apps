022a05be16ac110a142926ab8c3961df0b8734a6
alifzr31 <alifzakya.r@gmail.com>
2023-06-28 14:22:54 +0700

    fixing done detail shopping

--- lib/app/modules/shopping/shopping_detail/detail_shopping_modal.dart
+++ lib/app/modules/shopping/shopping_detail/detail_shopping_modal.dart
@@ -86,6 +86,7 @@ class _DetailShoppingModalState extends State<DetailShoppingModal> {
                       const SizedBox(height: 10),
                       Expanded(
                         child: ListView.builder(
+                          physics: const ClampingScrollPhysics(),
                           itemCount: controller.shoppingDetail.length,
                           itemBuilder: (context, index) {
                             final shoppingDetail =
