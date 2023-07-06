817a95369e900b82f1d91b09a546b4f442f779bb
alifzr31 <alifzakya.r@gmail.com>
2023-07-06 09:41:20 +0700

    add text align

--- lib/app/component/white_text.dart
+++ lib/app/component/white_text.dart
@@ -5,17 +5,20 @@ class WhiteText extends StatelessWidget {
     Key? key,
     required this.text,
     this.size,
+    this.textAlign,
     this.bold,
   }) : super(key: key);
 
   final String text;
   final double? size;
+  final TextAlign? textAlign;
   final FontWeight? bold;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      textAlign: textAlign,
       style: TextStyle(color: Colors.white, fontSize: size, fontWeight: bold),
     );
   }
